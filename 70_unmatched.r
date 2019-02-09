9a2524e5edee6e03a22499f1dc9c10a119436901
belerico <belo.fede@outlook.com>
2019-02-09 21:55:39 +0100

    Added plot of unmatched for recipes and continent

--- 70_unmatched.r
+++ 70_unmatched.r
@@ -9,15 +9,35 @@ for (i in seq_along(not_start_food)) {
     for (j in seq_along(not_start_food_elems)) {
       not_mapped_label = expl_data$mapped_ingredients[i][[1]][not_start_food_elems[j]]
       if (!(not_mapped_label %in% ingrs)) {
-        print(not_mapped_label)
+        # print(not_mapped_label)
         ingrs = append(ingrs, not_mapped_label)
         not_mapped = append(not_mapped, i)
       }
     }
   }
 }
+
+### Plot distribution per continent
 distr = as.data.frame(table(expl_data$cuisine[not_mapped]))
 labels = distr$Var1
-mp = barplot(distr$Freq, axes = FALSE, axisnames = FALSE, ylim=c(0,20))
-text(mp, par("usr")[3], labels = labels, srt = 60, adj = c(1.1,1.1), xpd = TRUE, cex=.9)
+mp = barplot(distr$Freq, axes = FALSE, axisnames = FALSE, ylim=c(0,20), main = "Unmatched ingredients per recipes")
+text(mp, par("usr")[3], labels = labels, srt = 45, adj = c(1.1,1.1), xpd = TRUE, cex=.9)
 axis(2)
+
+### Plot distribution per cuisine
+asian = c("japanese", "vietnamese", "korean", "thai", "chinese", "filipino", "indian")
+north_american = c("jamaican", "cajun_creole", "mexican")
+south_american = c("southern_us", "brazilian")
+mediterranean = c("greek", "italian", "moroccan", "spanish")
+european = c("british", "french", "irish", "russian")
+expl_data$cuisine[which(expl_data$cuisine %in% asian)] = "asian"
+expl_data$cuisine[which(expl_data$cuisine %in% north_american)] = "north_american"
+expl_data$cuisine[which(expl_data$cuisine %in% south_american)] = "south_american"
+expl_data$cuisine[which(expl_data$cuisine %in% mediterranean)] = "mediterranean"
+expl_data$cuisine[which(expl_data$cuisine %in% european)] = "european"
+distr = as.data.frame(table(expl_data$cuisine[not_mapped]))
+labels = distr$Var1
+mp = barplot(distr$Freq, axes = FALSE, axisnames = FALSE, ylim=c(0,40), main = "Unmatched ingredients per continent")
+text(mp, par("usr")[3], labels = labels, srt = 45, adj = c(1.1,1.1), xpd = TRUE, cex=.9)
+axis(2)
+
