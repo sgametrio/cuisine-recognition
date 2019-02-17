85eeade341e13fea87f6a2646eff048e28435385
belerico <belo.fede@outlook.com>
2019-02-17 23:08:54 +0100

    Now we compute also normalized edit and jaccard distance

--- combinedDistance.r
+++ combinedDistance.r
@@ -1,58 +1,35 @@
 # Compute combined distance
 require("stringr")
 require("stringdist")
-strs = c()
-str = readline("Inserire una stringa (per terminare inserire la stringa vuota): ")
-while (str != "") {
-  strs = append(strs, str)
-  str = readline("Inserire una stringa (per terminare inserire la stringa vuota): ")
-}
+# strs = c()
+# str = readline("Inserire una stringa (per terminare inserire la stringa vuota): ")
+# while (str != "") {
+#   strs = append(strs, str)
+#   str = readline("Inserire una stringa (per terminare inserire la stringa vuota): ")
+# }
 max_dist = as.integer(readline("Inserire la distanza di edit massima secondo cui due stringhe sono uguali: "))
-strs = c("chicken consomme",
-         "low sodium chicken",
-         "cut up chicken",
-         "minced chicken",
-         "roast breast of chicken",
-         "fryers",
-         "bone in chicken thighs",
-         "sliced chicken",
-         "chicken pan drippings",
-         "bone-in chicken",
-         "chicken pieces",
-         "chicken schmaltz",
-         "broiler chicken",
-         "diced chicken",
-         "boneless skin on chicken thighs",
-         "2 1/2 to 3 lb. chicken, cut into serving pieces",
-         "Knorr Chicken Flavor Bouillon",
-         "knorr chicken flavor bouillon cube",
-         "boneless chicken skinless thigh",
-         "skinless chicken fillets",
-         "black chicken",
-         "free-range chickens",
-         "bone in skinless chicken thigh",
-         "boneless skinless chicken",
-         "boneless chicken",
-         "chicken in water",
-         "broiler",
-         "fryer chickens",
-         "broiler-fryer chicken",
-         "broiler-fryers",
-         "organic chicken",
-         "bone in skin on chicken thigh",
-         "knorr chicken flavor bouillon",
-         "chicken fillets",
-         "chicken",
-         "skinless chicken pieces",
-         "chicken-flavored soup powder",
-         "whole chicken",
-         "chicken and rice soup")
+strs = c("wildflower honey",
+         "honey dijon mustard",
+         "tupelo honey",
+         "orange blossom honey",
+         "raw honey",
+         "clover honey",
+         "honey-flavored greek style yogurt",
+         "clear honey",
+         "country crock honey spread",
+         "honey",
+         "runny honey",
+         "liquid honey",
+         "honey whiskey")
 # In this simple version we only apply the tolower transformation
 # We might want to remove punctuation, numbers, strange symbols and so on
 strs = tolower(strs)
-distances = matrix(nrow = length(strs), ncol = length(strs))
-diag(distances) = 0
-colnames(distances) = rownames(distances) = strs
+# Compute the max string length to normalize the edit distance
+max_str_length = max(sapply(strs, str_length))
+combined_distances = edit_distances = jaccard_distances = matrix(nrow = length(strs), ncol = length(strs))
+diag(edit_distances) = diag(jaccard_distances) = diag(combined_distances) = 0
+colnames(combined_distances) = colnames(jaccard_distances) = colnames(edit_distances) = strs
+rownames(combined_distances) = rownames(jaccard_distances) = rownames(edit_distances) = strs
 # We compute a triangular distance matrix
 for (i in seq_len(length(strs)-1)) {
   for (j in (i+1):length(strs)) {
@@ -74,30 +51,52 @@ for (i in seq_len(length(strs)-1)) {
         }
       }
     }
+    # Edit distance
+    edit_distances[i, j] = stringdist(strs[i], strs[j], method = "lv")
+    # Jaccard distance
+    jaccard_distances[i, j] = stringdist(strs[i], strs[j], method = "jaccard")
     # Compute combined Jaccard distance
-    distances[i, j] = 1 - (length(intersection) / (length(tks1) + length(tks2) - length(intersection)))
+    combined_distances[i, j] = 1 - (length(intersection) / (length(tks1) + length(tks2) - length(intersection)))
   }
 }
-View(distances)
-# Pick element with the least average distance from all others elements
-selected_elem = 0
-min_avg = Inf
-sum = 0
+edit_distances = edit_distances / max_str_length
+# View(edit_distances)
+# View(jaccard_distances)
+# View(combined_distances)
+# Pick the element with the least average distance from all others elements, for three distance measure used
+selected_elem_edit = selected_elem_jaccard = selected_elem_combined = 0
+min_avg_edit = min_avg_jaccard = min_avg_combined = Inf
+sum_edit = sum_jaccard = sum_combined = 0
 for (i in seq_len(length(strs))) {
-  sum = 0
+  sum_edit = sum_jaccard = sum_combined = 0
   for (j in seq_len(length(strs))) {
     # Since we've first built a tringular matrix, we've to take the lowerside matrix element in the upperside
     if (j < i) {
-      sum = sum + distances[j, i]
+      sum_edit = sum_edit + edit_distances[j, i]
+      sum_jaccard = sum_jaccard + jaccard_distances[j, i]
+      sum_combined = sum_combined + combined_distances[j, i]
     } else if (i != j) {
-      sum = sum + distances[i, j]
+      sum_edit = sum_edit + edit_distances[i, j]
+      sum_jaccard = sum_jaccard + jaccard_distances[i, j]
+      sum_combined = sum_combined + combined_distances[i, j]
     }
   }
-  avg = sum / (length(strs)-1)
-  if (avg < min_avg) {
-    min_avg = avg
-    selected_elem = i
+  avg_edit = sum_edit / (length(strs)-1)
+  avg_jaccard = sum_jaccard / (length(strs)-1)
+  avg_combined = sum_combined / (length(strs)-1)
+  if (avg_edit < min_avg_edit) {
+    min_avg_edit = avg_edit
+    selected_elem_edit = i
   }
-  print(paste("Element", i, ":", strs[i], "; AVG: ", avg))
+  if (avg_jaccard < min_avg_jaccard) {
+    min_avg_jaccard = avg_jaccard
+    selected_elem_jaccard = i
+  }
+  if (avg_combined < min_avg_combined) {
+    min_avg_combined = avg_combined
+    selected_elem_combined = i
+  }
 }
-print(paste("Selected element: ", strs[selected_elem], "; AVG: ", min_avg))
+print(paste("Selected element using normalized edit distance: ", strs[selected_elem_edit], "; AVG: ", min_avg_edit))
+print(paste("Selected element using jaccard distance: ", strs[selected_elem_jaccard], "; AVG: ", min_avg_jaccard))
+print(paste("Selected element using combined distance: ", strs[selected_elem_combined], "; AVG: ", min_avg_combined))
